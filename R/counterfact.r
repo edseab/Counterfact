cbf3b2be613b67196e24fccbad127960ae662293
Ed Seabright <ed.seabright@gmail.com>
2019-11-22 07:27:26 -0800

    fix progress bar

--- R/counterfact.r
+++ R/counterfact.r
@@ -1,16 +1,13 @@
 
-counterfact <- function (object, x, range = "standard", n=1000, default = mean, CI=FALSE, PI=FALSE, other=NULL, CIsims = 1000, PIsims= 1000, conf=95, odds.to.prob=F, data=summary(object)$call$data, progress="none",...){
+counterfact <- function (object, x, range = "standard", n=1000, default = mean, CI=FALSE, PI=FALSE,
+other=NULL, CIsims = 1000, PIsims= 1000, conf=95, odds.to.prob=F, data=summary(object)$call$data, progress="none",...){
 
-
-progress <- function (i,length, percent=1) {
-	if(i==1){print(0)}
-	if(trunc(i*100/length) %% percent ==0 & trunc(i*100/length)!=trunc((i-1)*100/length)){ print(trunc(i*100/length))}
-}
 
 # Classic errors
-if(is.null(data)) stop("Model data must be inputed as data frame")
-if(!is.character(x)) stop("x must be inputed as character object")
-
+if (is.null(data)) stop("Model data must be inputed as data frame")
+if (!is.character(x)) stop("x must be inputed as character object")
+if (!is.null(other) & !is.list(other)) stop("other variables must be inputed as list")
+		
 d <- get(as.character(data))
 # if (any(apply(d,2,function(x) !is.numeric(x)))){
 	# stop("Non numeric variable - choose counterfactual value")}
@@ -35,21 +32,20 @@ if (length(factors)>0){
 	}
 
 nd <- unlist(lapply(d[,which(!(colnames(d) %in% factors))],function(x)f(x,na.rm=T)))
-if(length(factors)>0) {for (i in 1:length(factors)){
+if (length(factors)>0) {for (i in 1:length(factors)){
 	nd <- append(nd, NA, which(colnames(d)==factors[i])-1)}
 	names(nd)[which(colnames(d) %in% factors)]<-factors}
 
-
+if (!is.null(other) & any(!(names(other) %in% coefs))) {
+	wrong <- names(other)[which(!(names(other) %in% coefs))]
+	ifelse (length(wrong)>1, stop (paste("Variables", wrong, "not found in model")),
+							 stop (paste("Variable", wrong, "not found in model"))
+			)              
+}
 if (!is.null(other)) {
-	if (!is.list(other)) stop ("other variables must be inputed as list")
-	if (any(!(names(other) %in% coefs))) {
-		wrong <- names(other)[which(!(names(other) %in% coefs))]
-		if (length(wrong)>1) stop (paste("Variables", wrong, "not found in model"))
-		if (length(wrong)==1) stop (paste("Variable", wrong, "not found in model"))
-		}
 	ph <- other[which(names(other) %in% coefs)]
 	nd [match(names(ph), coefs)] <- as.vector(unlist(ph))
-	}
+}
 nd <- data.frame(matrix(nd,n,length(nd),byrow=T))
 nd[,which(!(colnames(d) %in% factors))] <- unlist(lapply(as.list(nd)[which(!(colnames(d) %in% factors))],function(.)as.numeric(as.character(.))))
 nd[,which(colnames(d) %in% factors)] <- unlist(lapply(data.frame(nd[which(colnames(d) %in% factors)]),as.character))
@@ -79,7 +75,7 @@ colnames (pr) [2] <- "predicted.mean"
 
 if (CI) {
 if(class(object)[1]=="lmerMod" | class(object)[1]=="glmerMod"){ predFun<-function(.) mm%*%fixef(.)
-bb<-bootMer(object,FUN=predFun,nsim=CIsims)
+bb<-bootMer(object,FUN=predFun,nsim=CIsims,.progress=progress)
 bb_se<-apply(bb$t,2,function(x) x[order(x)])
 lo <- which.min(abs(1:CIsims - (100-conf)*CIsims/200))
 hi <- CIsims - lo
---
+++ tests/testthat/test_lm.r
@@ -0,0 +1,21 @@
+
+
+test_that("basic linear model works", {
+	N<-25
+	a<- 6
+	x1<-runif(N,-100,100)
+	b1<- 2
+	x2<-rbinom(N,20,0.2)
+	b2<-9
+	x3<-rnorm(N,20,50)
+	b3<- -4
+	se<-50
+	y<- a + b1*x1 + b2*x2 + b3*x3 +rnorm(N,0,se)
+	d<- as.data.frame(cbind(y,x1,x2,x3))
+	mod<-lm(y~x1+x2+x3,data=d)
+	expect_silent(out <- counterfact(mod, x="x1", CI=T, other=list(x2=21)))
+	expect_equal(nrow(out), 1000)
+	
+	expect_error(counterfact(mod, x="x1", CI=T, other=list(nonsense=21)))
+	
+})

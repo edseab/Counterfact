c5407e8c90af632a5468343bd5b07d83c8c461c7
Ed Seabright <ed.seabright@gmail.com>
2021-09-03 15:13:00 -0700

    i did it again

--- R/load_args.r
+++ R/load_args.r
@@ -1,8 +1,8 @@
 load_args <- function(ph,func=NULL){
 ph[!sapply(ph,is.numeric)] <- paste0("'",ph[!sapply(ph,is.numeric)],"'")
 if(!is.null(func))ph <- ph[names(ph) %in% names(formals(func))]
-for(i in 1:length(ph))eval(parse(text=paste(names(ph),ph,sep="<-")[i]))
-print(paste(names(ph),ph,sep="<-")[i])
+for(i in 1:length(ph)){eval(parse(text=paste(names(ph),ph,sep="<-")[i]))
+print(paste(names(ph),ph,sep="<-")[i])}
 print("Done")
 
 }

89c0b92ed926723bd1c1d6038972168e3405d619
Ed Seabright <ed.seabright@gmail.com>
2021-09-03 15:01:17 -0700

    oops

--- R/load_args.r
+++ R/load_args.r
@@ -1,4 +1,4 @@
-load_args <- function(x,func=NULL){
+load_args <- function(ph,func=NULL){
 ph[!sapply(ph,is.numeric)] <- paste0("'",ph[!sapply(ph,is.numeric)],"'")
 if(!is.null(func))ph <- ph[names(ph) %in% names(formals(func))]
 for(i in 1:length(ph))eval(parse(text=paste(names(ph),ph,sep="<-")[i]))
